dbaeed1f2a234db42f6b222462886d507078ffd3
simonthi <github@simonthiefes.de>
2023-09-14 16:50:09 +0200

    Output is now otf

--- font/font_baker.py
+++ font/font_baker.py
@@ -1,18 +1,17 @@
 import fontforge
 import os
 
-font = fontforge.open('font/blank.sfd')
+
 
 with open("font/temp.txt", 'r') as f:
     char = f.read()
-
-directory = "mpost/output-svg"
-
 print(char)
 
+font = fontforge.open('font/blank.sfd')
+
 glyph = font.createChar(-1, char)
-glyph.importOutlines(''+ directory + "/65.svg")
+glyph.importOutlines('mpost/output-svg/66.svg').simplify()
 glyph.right_side_bearing = int(glyph.left_side_bearing)
 
 
-font.generate('font/testfont.ttf')
+font.generate('font/testfont.otf')
