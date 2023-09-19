3f6c987e3de501b2ef0abb6c8aece2ae8ae86554
simonthi <github@simonthiefes.de>
2023-09-19 15:34:52 +0200

    Making cleaner debian compatible

--- cleaner.sh
+++ cleaner.sh
@@ -1,1 +1,5 @@
-sed -i "" -e "s/<path.*style=\"stroke:rgb(100.000000%,0.000000%,0.000000%);.*fill: none;\"><\/path>//" mpost/output-svg/65.svg
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i "" -e "s/<path.*style=\"stroke:rgb(100.000000%,0.000000%,0.000000%);.*fill: none;\"><\/path>//" mpost/output-svg/65.svg
+else
+    sed -i -e "s/<path.*style=\"stroke:rgb(100.000000%,0.000000%,0.000000%);.*fill: none;\"><\/path>//" mpost/output-svg/65.svg
+fi
