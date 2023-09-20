86c389ab4c1a915e17ac59dea6c1e0bc81708347
simonthi <github@simonthiefes.de>
2023-09-20 12:48:44 +0200

    Tweaks in char_creation

--- font/font_baker.py
+++ font/font_baker.py
@@ -17,17 +17,21 @@ def fea_writer(table, glyph, chars):
     glyph.addPosSub(table, sub)
 
     
-    
+
 with open("font/temp.txt", 'r') as f:
     char = f.read()
 print(char)
 char_uni = int(get_uni(char), 16)
 print(char_uni)
+print(len(char))    
 
-font = fontforge.open('font/blank.sfd')
+font = fontforge.open('font/Computer-Vision.sfd')
 
-if(len(char) > 1):
-    if(font.createChar(-1, char) != -1):
+if(len(char) == 1):
+    sys.exit()
+
+if(len(char) > 2):
+    if(font.findEncodingSlot(char) != -1):
         print("Alert")
         counter = 1
         while(font.findEncodingSlot(char+".alt"+str(counter)) != -1):
@@ -40,19 +44,19 @@ if(len(char) > 1):
     fea = fea_writer("liga-1", glyph, char)
 
 else:
-    if(font.createChar(-1, char_uni) != -1):
+    if(font.findEncodingSlot(char_uni) != -1):
         print("Alert")
         counter = 1
-        while(font.findEncodingSlot(char_uni+".alt"+str(counter)) != -1):
+        while(font.findEncodingSlot(char+".alt"+str(counter)) != -1):
             counter = counter + 1
-        glyph = font.createChar(-1, char_uni+".alt"+str(counter))
+        glyph = font.createChar(-1, char+".alt"+str(counter))
     else:
-        glyph = font.createChar(-1, char_uni)
+        glyph = font.createChar(char_uni)
     glyph.importOutlines('mpost/output-svg/65.svg')
     glyph.right_side_bearing = int(glyph.left_side_bearing)
     
 
-font.save('font/blank.sfd')
-font.generate('font/testfont.otf')
+font.save('font/Computer-Vision.sfd')
+font.generate('font/Computer-Vision.otf')
 
 
